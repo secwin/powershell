881972465dde8d03f058befb851819cf947d841f
secwin <46894712+secwin@users.noreply.github.com>
2019-01-22 11:28:15 +0300

    Get logs remote computer

--- Get-WinEventWithin.ps1
+++ Get-WinEventWithin.ps1
@@ -8,3 +8,5 @@ $FilterTable = @{
 }
 
 Get-WinEvent -ComputerName $ComputerName -FilterHashtable $FilterTable -ErrorAction 'SilentlyContinue'
+
+<#Kullanımı/Usage:  .\Get-WinEventWithin.ps1 -ComputerName <t>IP or Computer Name </t> -StartTimestamp 'mm.dd.yyyy hh:mm' -EndTimestamp 'mm.dd.yyyy hh:mm'  #>
