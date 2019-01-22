47b41a7d668c313c05d6fd3932673e4625db772b
secwin <46894712+secwin@users.noreply.github.com>
2019-01-22 11:24:49 +0300

    Add files via upload

---
+++ Get-WinEventWithin.ps1
@@ -0,0 +1,10 @@
+﻿param([string] $ComputerName = 'localhost',[datetime]$StartTimestamp,[datetime]$EndTimestamp)
+
+$logs = (Get-WinEvent -ListLog * -ComputerName $ComputerName | where {$_.RecordCount}).Logname
+$FilterTable = @{
+        'StartTime' = $StartTimestamp
+        'EndTime' = $EndTimestamp
+        'LogName' = $logs
+}
+
+Get-WinEvent -ComputerName $ComputerName -FilterHashtable $FilterTable -ErrorAction 'SilentlyContinue'
