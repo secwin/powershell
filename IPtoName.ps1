68e00398e1d9da4a4be804618a0d4ccb0703815c
secwin <46894712+secwin@users.noreply.github.com>
2019-01-22 14:25:43 +0300

    Resolve IP list or group of IP to Hostname

--- IPtoName.ps1
+++ IPtoName.ps1
@@ -1,21 +1,23 @@
-#$listofIPs = Get-Content c:\IPList.txt
+#$listofIPs = Get-Content c:\user\X\Desktop\IPlist.txt
 
 $listofIPs = "<b>IP1</b>","<b>IP2</b>","<b>IP3</b>"
 $ResultList = @()
-foreach ($ip in $listofIPs)
 
-{
-
-     $result = $null
-     $currentEAP = $ErrorActionPreference
-     $ErrorActionPreference = "silentlycontinue"
-     $result = [System.Net.Dns]::gethostentry($ip)
-     $ErrorActionPreference = $currentEAP
-	 $Resoved = [string]$Result.HostName
-     If ($Result)
-     {$Resultlist += "$IP - $Resoved" }
-     else{$Resultlist += "$IP - No HostNameFound"}
-
-}
-#$resultlist | Out-File c:\output.txt
+foreach ($ip in $listofIPs){
+	$result = $null
+	$currentEAP = $ErrorActionPreference
+	$ErrorActionPreference = "silentlycontinue"
+	$result = [System.Net.Dns]::gethostentry($ip)
+	$ErrorActionPreference = $currentEAP
+	$Resoved = [string]$Result.HostName
+If ($Result){$Resultlist += "$IP - $Resoved" }
+else{$Resultlist += "$IP - Not fount hostname"}}
+#$resultlist | Out-File c:\user\X\Desktop\output.txt
 $ResultList
+
+#With this script you can resolve hostname from list or group of IP addresses
+#You can;
+#1) Add your IP addresses to "$listofIPs = "<b>IP1</b>","<b>IP2</b>","<b>IP3</b>""
+#or
+#1) Change path of IP list path from "$listofIPs = Get-Content <b>c:\user\X\Desktop\IPlist.txt</b>" code
+#2) Change path of result path from "$resultlist | Out-File <b>c:\user\X\Desktop\output.txt</b>" code
