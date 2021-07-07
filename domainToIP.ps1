337d075faafe337157d55b65cf9a66d9ab6ae48e
Ali Kaplan <46894712+secwin@users.noreply.github.com>
2021-07-07 14:40:24 +0300

    Add files via upload

---
+++ domainToIP.ps1
@@ -0,0 +1,43 @@
+$servers = get-content "C:\powershell\domains.txt"
+
+$serversAndIps ="C:\powershell\IPlist.csv"
+
+ 
+
+$results = @()
+
+foreach ($server in $servers)
+
+{
+
+    $result = "" | Select ServerName , ipaddress
+
+    $result.ipaddress = [System.Net.Dns]::GetHostAddresses($server)
+
+    $addresses = [System.Net.Dns]::GetHostAddresses($server)
+
+ 
+
+    foreach($a in $addresses)
+
+    {
+
+        "{0},{1}" -f $server, $a.IPAddressToString
+
+        $result.ipaddress = [System.Net.Dns]::GetHostAddresses($server)
+
+    }
+
+ 
+
+    $result.servername = $server
+
+    $result.ipaddress = $a.IPAddressToString
+
+    $results += $result
+
+}
+
+ 
+
+$results | export-csv -NoTypeInformation $serversandips
